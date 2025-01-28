c8f419314a3014ea76bd0647525cff16d2b3b0ff
myrari <myra@myrari.net>
2025-01-27 22:42:16 -0800

    worked on cue structs, not much display logic

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,87 @@
+use std::collections::HashSet;
+
+#[derive(Debug)]
 pub struct CueballApp {
     state: AppState,
 }
 
-pub struct AppState {
-    project: String,
+#[derive(Debug)]
+struct AppState {
+    project: Project,
+}
+
+#[derive(Debug)]
+struct Project {
+    name: String,
+
+    cues: CueList,
+}
+
+#[derive(Debug)]
+struct CueList {
+    list: Vec<Cue>,
+}
+
+impl CueList {
+    fn new() -> Self {
+        Self { list: vec![] }
+    }
+
+    fn add(&mut self, cue_type: CueType) {
+        let cue = Cue {
+            id: self.get_new_cue_id(),
+            name: match &cue_type {
+                CueType::Message(msg) => msg.clone(),
+                CueType::Process => String::from("Process cue"),
+            },
+            cue_type,
+        };
+        self.list.push(cue);
+    }
+
+    fn get_new_cue_id(&self) -> u64 {
+        let mut largest_id = 0;
+
+        for cue in &self.list {
+            if cue.id > largest_id {
+                largest_id = cue.id;
+            }
+        }
+
+        largest_id + 1
+    }
+}
+
+#[derive(Debug)]
+struct Cue {
+    id: u64,
+    name: String,
+
+    cue_type: CueType,
+}
+
+#[derive(Debug)]
+enum CueType {
+    Message(String),
+    Process,
 }
 
 impl Default for CueballApp {
     fn default() -> Self {
-        Self {
+        let mut app = CueballApp {
             state: AppState {
-                project: String::from("Untitled.cbp"),
+                project: Project {
+                    name: String::from("Untitled.cbp"),
+                    cues: CueList::new(),
+                },
             },
-        }
+        };
+
+        app.state.project.cues.add(CueType::Message(String::from("hi")));
+        app.state.project.cues.add(CueType::Message(String::from("there")));
+        app.state.project.cues.add(CueType::Message(String::from("tucker")));
+
+        app
     }
 }
 
@@ -25,7 +94,6 @@ impl CueballApp {
 
 impl eframe::App for CueballApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
         // top bar
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // menu bar
@@ -47,7 +115,15 @@ impl eframe::App for CueballApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Cueball");
 
-            ui.label("text");
+            cue_list_ui(ui, &self.state.project);
         });
     }
+}
+
+fn cue_list_ui(ui: &mut egui::Ui, project: &Project) -> () {
+    ui.vertical(|ui| {
+        for cue in &project.cues.list {
+            ui.label(cue.name.clone());
+        }
+    });
 }
