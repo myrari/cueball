f37062d9f146a56c0b55d1f17150e49b8084b3bb
Tucker R. Twomey <tucker@the-twomeys.com>
2025-02-03 19:48:03 -0800

    Also export Cue trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod app;
 mod data;
 
 pub use app::{AppState, CueballApp, Project};
-pub use data::{CueList, CueRunnable, CueTimed, RemarkCue};
+pub use data::{CueList, Cue, CueRunnable, CueTimed, RemarkCue};
