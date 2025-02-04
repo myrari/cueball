df5353d1253b4f15e5fc3059027cc8bb86830b84
Tucker R. Twomey <tucker@the-twomeys.com>
2025-02-03 18:06:59 -0800

    Add multiple modes to CLI

--- src/bin/cueball-cli.rs
+++ src/bin/cueball-cli.rs
@@ -12,10 +12,15 @@ impl std::fmt::Display for CLIMode {
 }
 
 fn main() -> Result<(), ()> {
-    let mut line_editor = Reedline::create();
+    let mut line_editor_cli = Reedline::create();
+    let mut line_editor_lua = Reedline::create();
     let mut mode = CLIMode::CLI;
 
     loop {
+        let mut line_editor = match mode {
+            CLIMode::CLI => &mut line_editor_cli,
+            CLIMode::Lua => &mut line_editor_lua
+        };
         let prompt = DefaultPrompt::new(
             DefaultPromptSegment::Basic(format!("Cueball {}",
                 mode.to_string())),
@@ -23,7 +28,14 @@ fn main() -> Result<(), ()> {
         let sig = line_editor.read_line(&prompt);
         match sig {
             Ok(Signal::Success(buffer)) => {
-                println!("{}", buffer);
+                match buffer.as_str() {
+                    "/cli" => mode = CLIMode::CLI,
+                    "/lua" => mode = CLIMode::Lua,
+                    "/exit" => break,
+                    ""      => (),
+                    _ => {
+                    }
+                }
             }
             Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
                 println!("\nAborted!");
